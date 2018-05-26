0ad2eba73fa35eea2fa12a28538bdc9d003526b7
Raninatadianastuti <39473500+Raninatadianastuti@users.noreply.github.com>
2018-05-26 10:36:11 +0700

    Update caesar-cipher.php

--- caesar-cipher.php
+++ caesar-cipher.php
@@ -1,65 +1,53 @@
 <html>
-<head>
-  
-</head>
-<body bgcolor="#ADE8E6">
+  <head>
+    <title>Enkripsi dan Dekripsi</title>
+  </head>
 
-<center>
-<h2><u><b>ROT13 : Caesar Cipher</b></u></h2>
-<h3>Enkripsi dan Dekripsi</h3>
-    <form action="" method="get">
-      Kalimat : <input type="text" name="kalimat">
-      <br><br>
-      <input type="submit" value="kirim">
-      <input type="reset" value="ulangi">
-    </form>
-</center>
-
-<?php
-/**
- * Caesar Cipher
- */
-$msg = $_GET['kalimat'];
-
-$key = 13;
-
-$mode = 'decrypt';
-
-$letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-$trans = '';
-
-$msg = strtoupper($msg);
-
-for ($i = 0; $i < strlen($msg); $i++) {
-    if (strpos($letters, $msg[$i]) !== false) {
-        $num = strpos($letters, $msg[$i]);
-        
-        if ($mode == 'encrypt') {
-            $num += $key;
-        } else {
-            $num -= $key;
-        }
-        
-        $cnt = strlen($letters);
-        if ($num >= $cnt) {
-            $num -= $cnt;
-        } else {
-            $num += $cnt;
-        }
-        if ($num >= $cnt) {
-            $num = $num - $cnt;
-        }
-        $trans .= $letters[$num];
-
-    }    
-}
-?>
-<center>
-<?php
-echo 'Hasil : ';
-echo $trans . PHP_EOL;
-?>
-</center>
-</body>
+   <body bgcolor="#ADE8E6">
+      <center>
+      <h2><u><b>ROT13 : Caesar Cipher</b></u></h2>
+      <h3>Enkripsi dan Dekripsi</h3>
+      <form action="" method="get">
+          Kalimat : <input type="text" name="kalimat">
+          <br><br>
+          <input type="submit" value="kirim">
+          <input type="reset" value="ulangi">
+      </form>
+      </center>
+      <?php
+          $msg = $_GET['kalimat'];
+          $key = 13;
+          $mode = 'decrypt';
+          $letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+          $trans = '';
+          $msg = strtoupper($msg);
+          for ($i = 0; $i < strlen($msg); $i++) {
+              if (strpos($letters, $msg[$i]) !== false) {
+                  $num = strpos($letters, $msg[$i]);
+              if ($mode == 'encrypt') {
+                  $num += $key;
+              } else {
+                  $num -= $key;
+              }
+              
+              $cnt = strlen($letters);
+              if ($num >= $cnt) {
+                  $num -= $cnt;
+              } else {
+                  $num += $cnt;
+              }
+              if ($num >= $cnt) {
+                  $num = $num - $cnt;
+              }
+              $trans .= $letters[$num];
+              }    
+         }
+     ?>
+     <center>
+     <?php
+        echo 'Hasil : ';
+        echo $trans . PHP_EOL;
+     ?>
+     </center>
+   </body>
 </html>
